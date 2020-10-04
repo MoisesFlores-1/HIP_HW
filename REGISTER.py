0545c8a277d289c898ebd9e2498a746c8d947285
MoisesFlores-1 <flores.m.1254@gmail.com>
2020-10-04 16:58:21 -0700

    commit

--- REGISTER.py
+++ REGISTER.py
@@ -1,9 +1,5 @@
 sales_tax = 9.5
 #sales tax is defined here and can be changed 
-#Here is the where item prices are defined by user
-num1 = input('Enter Price:')
-num2 = input('Enter Price:')
-#Here is where the sum of the input values are calculated
-sum = float(num1) + float(num2)
-#Display the total price
-print("Your total price"sum)
+IP = input ("Enter Price") #The user inputs the price of the item here
+ while IP (= 0):
+ input("Enter Price")
