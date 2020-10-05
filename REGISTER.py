e718cfc22c94ed4ea6393f8b36921205ef74a3b5
MoisesFlores-1 <flores.m.1254@gmail.com>
2020-10-04 17:02:45 -0700

    commit

--- REGISTER.py
+++ REGISTER.py
@@ -1,5 +1,5 @@
 sales_tax = 9.5
 #sales tax is defined here and can be changed 
 IP = input ("Enter Price") #The user inputs the price of the item here
- while IP (= 0):
- input("Enter Price")
+while IP (=0)
+  input("Enter Price")
